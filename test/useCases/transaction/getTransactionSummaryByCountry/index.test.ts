393cba5b865c6c733cc693986cdabbe0ed3e46f1
Daniela Osorio R <danosori@bancolombia.com.co>
2021-06-26 10:37:22 -0500

    Se cambia estructura de pruebas

--- test/useCases/transaction/getTransactionSummaryByCountry/index.test.ts
+++ test/useCases/transaction/getTransactionSummaryByCountry/index.test.ts
@@ -1,11 +1,16 @@
-
 import { expect } from 'chai';
 import { getTransactionSummaryService } from '../../../../src/useCases/transaction/getTransactionSummaryByCountry';
-import { TransactionSummary } from '../../../../src/entities/TransactionSummary';
+
 
 describe('getTransactionSummaryByCountry', () => {
-    it('getTransactionSummaryByCountry success', async() => {        
-        //const credit: Credit = await getCreditService.execute({"debenture":"87100001189","customerId":"CC-1026755666"});
-        expect(100000).to.equal(100000);       
+    it('getTransactionSummaryByCountry success', async() => {
+        try {
+            const transactionByYear = await getTransactionSummaryService.execute({country: "SD"});
+            console.log(transactionByYear);
+            expect(100000).to.equal(100000);
+            return;
+          } catch (error) {
+              console.log("mierror", error);
+          }
     });
 });
